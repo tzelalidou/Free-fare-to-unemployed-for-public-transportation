d846c703b9b7c1ee8f65c57bd786ead1867309b5
tzelalidou <dimitratzelalidou@gmail.com>
2022-01-08 14:00:58 +0200

    One more commit

--- springbootapplication-main/spring-boot-demo-2021/src/main/java/gr/hua/ds/springboot1/service/ApplicationService.java
+++ springbootapplication-main/spring-boot-demo-2021/src/main/java/gr/hua/ds/springboot1/service/ApplicationService.java
@@ -1,9 +1,14 @@
 package gr.hua.ds.springboot1.service;
 import gr.hua.ds.springboot1.entity.Application;
 import gr.hua.ds.springboot1.repository.ApplicationRepository;
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.stereotype.Service;
-        import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class ApplicationService {
@@ -15,7 +20,38 @@ public class ApplicationService {
         this.applicationRepository = applicationRepository;
     }
 
+    // get all the applications
     public List<Application> getApplications() {
         return applicationRepository.findAll();
+    }
+
+    // get all applications that need to be validated by OAED
+    public List<Application> getApplicationsOAED() {
+        return applicationRepository.findAllById(Collections.singleton(0));
+    }
+
+    // get all applications that need to be confirmed by OASA
+    public List<Application> getApplicationsOASA() {
+        return applicationRepository.findAllById(Collections.singleton(1));
+    }
+
+    // get an application by id
+    public Application getApplication(int id) {
+        return  applicationRepository.getById(id);
+    }
+
+    // save an application
+    public void saveApplication(Application application) {
+        applicationRepository.saveAndFlush(application);
+    }
+
+    // remove an application
+    public void removeApplication(Application application) {
+        applicationRepository.delete(application);
+    }
+
+    // remove application by id
+    public void removeApplicationById(int id) {
+        applicationRepository.deleteById(id);
     }
 }
--- springbootapplication-main/spring-boot-demo-2021/src/main/java/gr/hua/ds/springboot1/service/UserService.java
+++ springbootapplication-main/spring-boot-demo-2021/src/main/java/gr/hua/ds/springboot1/service/UserService.java
@@ -1,4 +1,7 @@
 package gr.hua.ds.springboot1.service;
+import gr.hua.ds.springboot1.entity.User;
+import gr.hua.ds.springboot1.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
@@ -6,7 +9,37 @@ import java.util.List;
 
 public class UserService {
 
+    private UserRepository userRepository;
 
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // get all the users
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    // get a user by id
+    public User getUser(int id) {
+        return userRepository.getById(id);
+    }
+
+    // save a user
+    public void saveUser(User usr) {
+        userRepository.saveAndFlush(usr);
+    }
+
+    // remove a user
+    public void removeUser(User usr) {
+        userRepository.delete(usr);
+    }
+
+    // remove a user by id
+    public void removeUserById(int id) {
+        userRepository.deleteById(id);
+    }
 }
 
 
